ace610ebe94e0ed2ba461d22fd013258d52940e8
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-29 22:14:59 +0300

    pets

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,23 @@
 class Pet:
-    pass
+    all=[]
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    def __init__(self,name,pet_type,owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self.__pet_type
+
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type in Pet.PET_TYPES:
+            self.__pet_type = pet_type
+        else:
+            raise Exception(f"{pet_type} is not a valid pet type")
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
